441e23b5f7cb96fae3e6e044bec552cc044546df
Abdullah Arıkan <bdllh.arikan@gmail.com>
2019-10-12 20:19:50 +0300

    Updates

---
+++ precache-manifest.52b7fc33e80021d212491c303469d06a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5adcfc2b04b5ffb76c3ac8257975e1f",
+    "url": "/react-form/index.html"
+  },
+  {
+    "revision": "ce11e92d50ebe5c2481a",
+    "url": "/react-form/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "269853c18fac73954667",
+    "url": "/react-form/static/js/2.8226fdb4.chunk.js"
+  },
+  {
+    "revision": "ce11e92d50ebe5c2481a",
+    "url": "/react-form/static/js/main.159d548a.chunk.js"
+  },
+  {
+    "revision": "a485f888f3ce3d0483be",
+    "url": "/react-form/static/js/runtime-main.445953d8.js"
+  }
+]);
