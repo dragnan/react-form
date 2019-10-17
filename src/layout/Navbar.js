fefa05a5edcbae988c7dfa7157aba5d98f6d4013
Abdullah Arıkan <bdllh.arikan@gmail.com>
2019-10-17 08:19:10 +0300

    home link

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 function Navbar({title}) {
     return (
         <nav className="navbar-nav navbar-expand-lg navbar-dark bg-dark mb-3 p-3">
-            <a href="/" className="navbar-brand">{title}</a>
+            <a href="/react-form" className="navbar-brand">{title}</a>
             <ul className="navbar-nav ml-auto">
                 <li className="nav-item active">
                     <Link to = "/react-form" className ="nav-link">Home</Link>
